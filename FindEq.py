838c27bd67826bdb4f494b4f33fe60ef33a2cf52
Brian Spencer <46761632+BrianASpencer@users.noreply.github.com>
2020-02-12 14:51:07 -0500

    Update FindEq.py

--- FindEq.py
+++ FindEq.py
@@ -1,96 +1,82 @@
-class Node:
-    def __init__(self, data):
-        self.left = None
-        self.right = None
-        self.data = data
-        if data == '+' or data == '-' or data == '*' or data == '/':
-            self.value = float('inf')
-            self.type = 'op'
-        else:
-            self.value = data
-            self.type = 'num'
+class Chromosome:
 
-    def insert(self, data, typ):
-        if self.data and typ == 'num':
-            if self.type == 'num':
-                if data < self.value:
-                    if self.left is None:
-                        self.left = Node(data)
-                    else:
-                elif data > self.value:
-                    if self.right is None:
-                        self.right = Node(data)
-                    else:
-                        self.right.insert(data)
-            else:
-                if self.left is None:
-                    self.left = Node(data)
-                else:
-                    self.right = Node(data)
-        elif self.data and typ = 'op':
-            if self.type == 'op':
-                if self.left is None:
-                    self.left = Node(data)
-                else:
-                    self.right = Node(data)
-            else:
-                if self.right is None:
-                    self.right = Node(data)
-                else:
-                    self.left.insert(data)
-        else:
-            self.data = data
-            self.type = typ
-            
-    def eval(root): 
-        if root is None: 
-            return 0
-        if root.left is None and root.right is None: 
-            return int(root.data) 
-        left_sum = evaluateExpressionTree(root.left) 
-        right_sum = evaluateExpressionTree(root.right)
-        
-        if root.data == '+': 
-            return left_sum + right_sum
-        
-        elif root.data == '-': 
-            return left_sum - right_sum 
-          
-        elif root.data == '*': 
-            return left_sum * right_sum 
-          
-        else: 
-            return left_sum / right_sum
+    operators = ['+', '-', '*', '/']
+    numbers = [-5, -4, -3, -2, -1, 0, 'x', 1, 2, 3, 4, 5]
 
-        
+    # constructor
+    def __init__(self):
+        self.tree = ['/', '+', '-', 1, 2, 5, '*', None, None, None, None, None, None, 3, 1]
+        self.expression = []
 
-
-if __name__ == '__main__':
-    root = Node('+')
-    root.insert(6, 'num')
-    root.insert('-', 'op')
-    root.insert(3, 'num')
-    root.insert(2, 'num')
-
-    #root.eval()
-    #https://medium.com/swlh/build-binary-expression-tree-in-python-36c04123e57b
-    #https://www.tutorialspoint.com/python_data_structure/python_binary_tree.htm
-
-### equally important consideration for tree
-    
 def childNodes(i):
-     return (2*i)+1, (2*i)+2
+    return (2 * i) + 1, (2 * i) + 2
 
-def traversed(a, i=0, d = 0):
+
+def traversed(a, i=0, d=0):
     if i >= len(a):
-        return 
-    l, r =  childNodes(i)
-    traversed(a, r, d = d+1)
-    print("   "*d + str(a[i]))
-    traversed(a, l, d = d+1)
+        return
+    l, r = childNodes(i)
+    traversed(a, r, d=d + 1)
+    if not str(a[i]) == "None":
+        print("   " * d + str(a[i]))
+    traversed(a, l, d=d + 1)
 
-a =  ['+', 6, '-', None, None, 12, 3]
+def inorder(a, i=0):
+    if i >= len(a):
+        return
+    l, r = childNodes(i)
+    inorder(a, r)
+    if str(a[i]) != "None":
+        print(str(a[i]))
+    inorder(a, l)
 
-traversed(a)
+def eval(a, i=0):
+    if a[i] is None:
+        return 0
+    l, r = childNodes(i)
+    if l is None and r is None:
+        return int(a[i])
+
+    print(i)
+
+    left_sum = eval(a, l)
+    right_sum = eval(a, r)
+
+    if a[i] == '+':
+        return left_sum + right_sum
+    elif a[i] == '-':
+        return left_sum - right_sum
+    elif a[i] == '*':
+        return left_sum * right_sum
+    elif a[i] == '/':
+        return left_sum / right_sum
+
 
 ###
+def main():
+    populationSize = 600
+    population = []
+    a = ['/', '+', '-', 1, 2, 5, '*', None, None, None, None, None, None, 3, 1]
+    for i in range(0, 1):
+        population.append(Chromosome())
+    traversed(population[0].tree)
+    inorder(population[0].tree)
+
+if __name__ == "__main__":
+    main()
+
+"""
+         1
+      *
+         3
+   -
+      5
+/
+      2
+   +
+      1
+      
+(1+2)/(5-(1*3))
+
+1+2/5-1*3
+"""
